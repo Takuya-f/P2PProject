ab44df84add48d38ef3a482ec680a4260026ecd2
Takuya-f <fukamachi@posl.ait.kyushu-u.ac.jp>
2015-01-28 01:19:42 +0900

    implemented for "A "state machine

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<Node> nodeList = new ArrayList<Node>();
-		Node n1 = makeNode("MyNode",nodeList);
-		Node n2 = makeNode("OtherNode",nodeList);
+		Node n1 = makeNode("MyNode");
 		n1.start();
 	}
 
-	private static Node makeNode(String nodeName,List<Node> list) {
+	private static Node makeNode(String nodeName) {
 		Node n1 = new Node(nodeName);
-		list.add(n1);
 		return n1;
 	}
 
--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Node {
 	private State state = State.IDLE;
 	private String name = null;
@@ -9,6 +11,16 @@ public class Node {
 	public boolean start() {
 		if (this.state == State.IDLE) {
 			System.out.println(this.name + " is start!");
+			this.state = State.LEECHING;
+			Scanner scan = new Scanner(System.in);
+			String str = scan.next();
+			if (str.equals("c")) {
+				cancel();
+			} else {
+				/* é¿ç€ÇÕDLèàóùÅCèIÇÌÇ¡ÇΩÇÁcompleted */
+				completed();
+			}
+			scan.close();
 			return true;
 		} else {
 			System.out.println(this.name + "'s state is not IDLE.");
@@ -17,10 +29,34 @@ public class Node {
 	}
 
 	public boolean cancel() {
+		if (this.state != State.IDLE) {
+			System.out.println(this.name + " is canceled.");
+			this.state = State.IDLE;
+			return true;
+		} else {
+			System.out.println(this.name + " is already idling.");
+		}
 		return false;
 	}
 
 	public boolean completed() {
+		if (this.state == State.LEECHING) {
+			System.out.println(this.name + "'s download is completed!");
+			this.state = State.SEEDING;
+			Scanner scan = new Scanner(System.in);
+			String str = scan.next();
+			while (true) {
+				if (str.equals("c")) {
+					cancel();
+					scan.close();
+					return true;
+				} else {
+					str = scan.next();
+				}
+			}
+		} else {
+			System.out.println(this.name + " is not downloading.");
+		}
 		return false;
 	}
 }
